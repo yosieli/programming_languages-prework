822861774521151acecbde3ad7674275dc81a57b
feven <feventsegay6@gmail.com>
2019-05-03 00:36:21 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
 def reformat_languages(languages)
-  # your code here
+new_languages = {}
+languages.each do |style, language|
+  language.each do |name, attributes|
+  new_languages[name] ||= attributes
+  new_languages[name][:style]=[]
+  new_languages[name][:style]<<style
 end
+end
+new_languages
+end
+
